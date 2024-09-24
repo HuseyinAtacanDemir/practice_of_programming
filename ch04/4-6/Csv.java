7adba4ca7d7d3ba530570de0242cc7475645d8f4
HuseyinAtacanDemir <40474534+HuseyinAtacanDemir@users.noreply.github.com>
2024-09-24 17:17:50 -0400

    ADD ch04 java implementation with StringBuilder reuse and preallocate arraylist

--- ch04/4-6/Csv.java
+++ ch04/4-6/Csv.java
@@ -3,17 +3,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Csv {
+    private final int PREALLOCATE = 16;
 
     private char    fieldSep;
     private String  line;
+
+    private StringBuilder field;
+    private int nField;
     private ArrayList<String> fields;
+
     private BufferedReader reader;
 
     Csv(BufferedReader reader, char sep) {
         this.reader = reader;
         this.line = null;
         this.fieldSep = sep;
-        this.fields = new ArrayList<String>();
+        this.nField = 0;
+        this.fields = new ArrayList<String>(this.PREALLOCATE);
+        this.field = new StringBuilder();
     }
 
     Csv(BufferedReader reader) {
@@ -34,13 +41,13 @@ public class Csv {
     }
     
     public String getField(int n) {
-        if (n >= 0 && n < fields.size())
+        if (n >= 0 && n < nField)
             return fields.get(n);
         return null;
     }
     
     public int getNfield() {
-        return fields.size();
+        return this.nField;
     }
 
     private void split() {
@@ -49,21 +56,24 @@ public class Csv {
             return;
 
         boolean quote = false;
-        StringBuilder field = new StringBuilder();
-
+        field.setLength(0);
+        nField = 0;
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
             if (c == '"') {
                 quote = !quote;
             } else if (c == ',' && !quote) {
-                fields.add(field.toString().trim());
+                fields.add(field.toString());
                 field.setLength(0);
+                nField++;
             } else { 
                 field.append(c);
             }
         }
-        if (field.length() > 0)
-            fields.add(field.toString().trim());
+        if (field.length() > 0) {
+            fields.add(field.toString());
+            nField++;
+        }
     }
     
 }
