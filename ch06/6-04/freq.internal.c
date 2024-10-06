0094cd9f0ab4b377790b7c5d46d8a97b1ced5776
HuseyinAtacanDemir <40474534+HuseyinAtacanDemir@users.noreply.github.com>
2024-10-06 17:09:12 -0400

    ADD ch06 6-04 freq freq.internal.c RAW_OPT_MASK TYPE_OPTS_MASK STRUCT_OPT_MASK descriptive comments

--- ch06/6-04/freq.internal.c
+++ ch06/6-04/freq.internal.c
@@ -10,10 +10,10 @@
 //#include "qsort.h"
 
 #define N_SUPPORTED_OPTS  10
-
-#define TYPE_OPTS_MASK    0x3E0
-#define RAW_OPT_MASK      0x008
-#define STRUCT_OPT_MASK   0x200
+                                  //  - - S l  f d i s  R D a h
+#define TYPE_OPTS_MASK    0x3E0   //  0 0 1 1  1 1 1 0  0 0 0 0
+#define RAW_OPT_MASK      0x008   //  0 0 0 0  0 0 0 0  1 0 0 0
+#define STRUCT_OPT_MASK   0x200   //  0 0 1 0  0 0 0 0  0 0 0 0
 
 #define DEFAULT_DELIM     ""
 #define OPT_STR           "+:haD:RsidflS:"
