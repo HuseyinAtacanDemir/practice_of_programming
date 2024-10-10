58b815501b5d3f4ce11b730f7b105657411aebff
HuseyinAtacanDemir <40474534+HuseyinAtacanDemir@users.noreply.github.com>
2024-10-10 02:09:15 -0400

    WIP ch06 6-04 jankunit jankunit.test.c FORK and utilities for testing exiting/printing functions proposals brainstorm

--- ch06/6-04/jankunit.test.c
+++ ch06/6-04/jankunit.test.c
@@ -11,7 +11,7 @@ int some_fn(int a, int b)
     if (!a || !b)
         eprintf(InvPosNumErr, a, b);
     printf(SumInfoOut, a, b, (a+b));
-    exit(EXIT_SUCCESS);
+    return a+b;
 }
 
 int main(void) {
@@ -56,6 +56,68 @@ int main(void) {
                     //}
                 //}
             //}
+//
+            ////OR
+//
+            //TEST("Should forked tests be like this?"){
+                //SHARED("iii", a, b, result, 5, 5, 0 );
+                //FORK("i", some_fn, result, a, b);
+//
+                //ASSERT_EQ(EXIT_CODE, 0);
+                //ASSERT_EQ(SIGNAL, 0);
+//
+                //EXPECT_STREQ(OUT, exp_out);
+                //EXPECT_STREQ(ERR, exp_err);
+//
+                //EXPECT_EQ(result, (a+b));
+            //}
+//
+            ////Or
+//
+            //TEST("I think it should be like this"){
+                //int a, b, result;
+                //a = b = 5;
+                //FORK(some_fn, result, a, b);
+//
+                //ASSERT_EXIT_EQ(0);
+                //ASSERT_SIG_EQ(0);
+//
+                //EXPECT_EQ(result, (a+b));
+//
+                //EXPECT_OUT_EQ(SumInfoOut, a, b, (a+b));
+                //EXPECT_ERR_EQ("");
+            //}
+
+// With this last case, I would need to be omniscient to know
+// exactly what the fn being tested is going to return, especially if
+// the return types have pointers in them, dynamically allocated data...
+// I cannot just make a shared memory pointer point to the private mem
+// space of a child process and expect any comparison to work in the parent
+// after the process exits. Therefore whatever checks the user implements,
+// meaning checks other than OUT ERR EXIT SIG comparisons, will have to
+// be made in the scope of the FORK, meaning shmem will only be used for
+// synchronizing the TEST/SUITE/PROG state after the child exits
+
+// so we demand knowledge of the internals of FORK from the user.
+// because it will be the users responsibility to test the non-shared
+// resources withint he FORK block
+
+// In that case, let's go all the way and only make EXIT and SIG tests
+// be conventional just outside following the FORK.
+
+// Thus we should handle the OUT and ERR buf checks within the FORK scope,
+// So we need to implement the piping logic within FORK
+
+// Since this will be complicated, perhaps instead of wrapping every aspect
+// of testing exiting/printing functions in macros, we might just give
+// utility functions to the user
+// So a hypothetical flow of using FORK on an exiting/printing function
+// could be like:
+
+            TEST("Proposed hypothetical FORK with utilities"){
+                
+
+            }
         }
     }
     return 0;
