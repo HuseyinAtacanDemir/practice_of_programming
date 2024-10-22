b1b5ea733f9500887d59b922bc45c71aeeeaeecb
HuseyinAtacanDemir <40474534+HuseyinAtacanDemir@users.noreply.github.com>
2024-10-22 00:32:14 -0400

    REFACTOR ch06 6-04 freq jankunit jankunit.assert.h changed the STREQ dependent EXPECT_ERR_EQ EXPECT_OUT_EQ ASSERT_ERR_EQ ASSERT_OUT_EQ macros to handle NULL strings directly, in accordance to the NULL support additions to the STREQ family of macros

--- ch06/6-04/jankunit.assert.h
+++ ch06/6-04/jankunit.assert.h
@@ -206,7 +206,7 @@
     do {  \
         char *_buf_ = NULL; \
         easprintf(&_buf_, __VA_ARGS__); \
-        comp(_buf_, ((GLOBAL_CTX->bufs && GLOBAL_CTX->bufs->bufs[USR][stream]) ? GLOBAL_CTX->bufs->bufs[USR][stream] : "")); \
+        comp(_buf_, (GLOBAL_CTX->bufs ? GLOBAL_CTX->bufs->bufs[USR][stream] : NULL)); \
         free(_buf_);  \
     } while (0)
 
