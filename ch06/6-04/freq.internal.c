96111316a706c46cb36743345a424b6ee91d8637
HuseyinAtacanDemir <40474534+HuseyinAtacanDemir@users.noreply.github.com>
2024-10-23 02:02:49 -0400

    REFACTOR ch06 6-04 freq freq.internal.c freq.internal.h added helper functions to parse opt to improve readablity, added a pseudo code implementation of the main logic of freq as a draft.

--- ch06/6-04/freq.internal.c
+++ ch06/6-04/freq.internal.c
@@ -5,11 +5,10 @@
 #include <string.h>
 #include <limits.h>
 #include <ctype.h>
-#include <assert.h>
 
 #include "eprintf.h"
 #include "hash.h"
-//#include "qsort.h"
+#include "qsort.h"
 
 const char *ErrOptReqsArg       = "option -%c requires an argument";
 const char *ErrOptMutex         = "Option -%c cannot be used with -%c";
@@ -33,7 +32,7 @@ const char *UsageInfoStr =
 "\t-d,        --double       Interpret input as doubles\n"
 "\t-S,        --string       Interpret input as strings";
 
-// struct option: defined in getopt.h, see man 3 getopt 
+// struct option: defined in getopt.h, see "man 3 getopt_long"
 const struct option LongOpts[] = {
     {"help",      no_argument,        0,  'h'},
     {"aggregate", no_argument,        0,  'a'},
@@ -51,85 +50,89 @@ void freq(FILE *fin, int flags, char *delim, int size)
 {
 		if (fin == NULL)
 				return;
-    return ;
+    /*
+    pseudocode:
+    create an dynamic array of structs with possible hmaps
+    if not raw {
+        for each line read {
+            if -c
+                iterate over line, increment relevant to char bucket
+            if other type options exist alongside -c, or no options were given from the getgo {
+                tokenize based on delim or whitespace
+                for each token {
+                    given -i && check if strtoi, check i first cause strtod will match
+                        create hmap_int if not alreadyadd to hmap_int
+                    else given -d, check if strtod(str, end) end-str == strlen
+                        add to hmap_double
+                    if none given, or explicit -S given:
+                        add the whole token to hmap_str
+                }
+            }
+        }
+    } else {
+        get size from a type option --if exists, or the size provided to raw as an arg
+        read into a buffer until EOF, making sure buffer is multiple of size {
+            create custom hmap with key size info
+            populate hmap by iterating over bytes with a buffer
+        }
+    }
+
+    if not aggregate or (aggregate and last file) {
+        if -c {
+            just print whole char buckets
+        }
+        if sort {
+            for all maps
+                create array of len n_elems of map
+                sort the array
+
+            print the sorted arrays in proper order:
+                in the order of option declaration
+        } else {
+            print regular traversal of maps in the order of option declaration
+        }
+        free all resources and nullify
+    }
+
+    */
+    return;
 }
 
 int parse_opts(int argc, char *argv[], char **delim, int *size) 
 {
     int opt, flags, n_raw_size_given, n_delim_given;
-    char *inv_opt_str = NULL;
 
-    flags = n_raw_size_given = n_delim_given = 0;
+    flags   = 0;
+    *delim  = DEFAULT_DELIM;
+    *size   = DEFAULT_SIZE;
 
-    *delim = DEFAULT_DELIM;
-    *size = DEFAULT_SIZE;
+    n_raw_size_given  = 0;
+    n_delim_given     = 0;
 
     // opterr: getopt.h, 0'ing it supresses getop.h errs, see "man 3 getopt"
     opterr = 0; 
     while ((opt = getopt_long(argc, argv, OPT_STR, LongOpts, NULL)) != -1) {
         // cases 'h' ':' and '?' exit the program
         switch (opt) {              
-            case 'h': usage_exit(); // EXIT_SUCCESS
+            case 'h': e_usage(); // EXIT_SUCCESS
             case 'a': break;
             case 's': break;
-            case 'D': *delim = estrdup(optarg); n_delim_given++; break;
-            case 'R': 
-                if (optarg) {
-                    n_raw_size_given++;
-                    if ((*size = atoi_pos(optarg)) == NOT_POSITIVE_INT_ERR)
-                        eprintf(ErrInvSizeArg, optarg);  // EXIT_FAILURE
-                }
-                break;
+            case 'D': e_set_delim(delim, &n_delim_given); break;
+            case 'R': e_set_raw_size(size, &n_raw_size_given); break;
             case 'c': // type options just set relevant flag bit
             case 'i':
             case 'd':
             case 'S': break;
+
             // optind: getopt.h, index of next arg, "man 3 getopt"
             case ':': eprintf(ErrOptReqsArg, optopt); // EXIT_FAILURE
-            case '?':
-                if (optopt) 
-                    easprintf(&inv_opt_str, "-%c", optopt);
-                else
-                    easprintf(&inv_opt_str, "%s", argv[optind-1]);
-                eprintf(ErrInvOpt, inv_opt_str);  // EXIT_FAILURE
-                
+            case '?': e_invalid_opt(argv);                
             // impossible case, EXIT_FAILURE
             default: eprintf("Unexpected option: %c:", opt);
         }
 		    flags = set_opt_flag(flags, opt);
     }
-    
-    // TODO: validation in a different function?
-    int type_opts = flags & TYPE_OPTS_MASK;
-    // binary algebraic expr results non-zero if more than 1 bit is set 
-    // more than one type opts with a raw option
-    if ((type_opts & (type_opts - 1)) && (flags & RAW_OPT_MASK))
-        eprintf(ErrMultiSizeRaw);
-    
-    // type opt was provided alongisde an explicit raw size
-    if (type_opts && n_raw_size_given)
-        eprintf(ErrMultiSizeRaw);
-    
-    // -R was used without optional size argument, but with no type option
-    if (!type_opts && (flags & RAW_OPT_MASK) && !n_raw_size_given)
-        eprintf(ErrOptMutexDefault);
-    
-    // -R was used multiple times with size information
-    if (n_raw_size_given > 1) 
-        eprintf(ErrDupOptArg, 'R');
-    
-    // -D was used multiple times with delim information
-    if (n_delim_given > 1)
-        eprintf(ErrDupOptArg, 'D');
-    
-    // cannot have delims on raw binary input
-    if ((flags & RAW_OPT_MASK) && (flags & DELIM_OPT_MASK))
-        eprintf(ErrOptMutex, 'D', 'R');
-    
-    // cannot have "raw" string, we handle ascii files, it's already raw
-    if ((flags & RAW_OPT_MASK) && (flags & STRING_OPT_MASK))
-        eprintf(ErrOptMutex, 'S', 'R');
-
+    e_validate_flags(flags, n_raw_size_given, n_delim_given); 
     return flags;
 }
 
@@ -143,12 +146,6 @@ int set_opt_flag(int flags, int opt)
 				if (LongOpts[i].val == opt)
 				    return ( flags | (1 << i) );
      return flags;
-}
-
-void usage_exit(void)
-{
-    weprintf(UsageInfoStr);
-    exit(EXIT_SUCCESS);
 }
 
 /* atoi_pos: convert a string to +integer, report if error */
@@ -168,3 +165,75 @@ int atoi_pos(char *str)
     return (int) result;
 }
 
+/* e_usage: print the UsageInfoStr and exit with EXIT_SUCCESS */
+void e_usage(void)
+{
+    weprintf(UsageInfoStr);
+    exit(EXIT_SUCCESS);
+}
+
+/* e_set_delim: tries to set delim from args, exits with message if err */
+void e_set_delim(char **delim, int *n_delim)
+{
+    *delim = estrdup(optarg);
+    (*n_delim)++;
+}
+
+/* e_set_raw_size: tries to set raw size from args, exits with msg if err */
+void e_set_raw_size(int *size, int *n_raw)
+{
+    if (optarg) {
+        (*n_raw)++;
+        if ((*size = atoi_pos(optarg)) == NOT_POSITIVE_INT_ERR)
+            eprintf(ErrInvSizeArg, optarg);  // EXIT_FAILURE
+    }
+}
+
+/* e_invalid_opt: in case of an invalid opt, print err msg and exit */
+void e_invalid_opt(char **argv)
+{
+    char *inv_opt_str_tmp = NULL;
+    if (optopt) 
+        easprintf(&inv_opt_str_tmp, "-%c", optopt);
+    else
+        easprintf(&inv_opt_str_tmp, "%s", argv[optind-1]);
+
+    eprintf(ErrInvOpt, inv_opt_str_tmp);  // EXIT_FAILURE
+}
+
+/* validate_flags: validates the flags set by options given, 
+                  if wrong usage, prints the specific err and exits */
+void e_validate_flags(int flags, int n_raw, int n_delim)
+{
+    int type_opts = flags & TYPE_OPTS_MASK;
+    // more than one type opts with a raw option
+    // binary algebraic expr results non-zero if more than 1 bit is set 
+    if ((type_opts & (type_opts - 1)) && (flags & RAW_OPT_MASK))
+        eprintf(ErrMultiSizeRaw);
+    
+    // type opt was provided alongisde an explicit raw size
+    if (type_opts && n_raw)
+        eprintf(ErrMultiSizeRaw);
+    
+    // -R was used without optional size argument, but with no type option
+    if (!type_opts && (flags & RAW_OPT_MASK) && !n_raw)
+        eprintf(ErrOptMutexDefault);
+    
+    // -R was used multiple times with size information
+    if (n_raw > 1) 
+        eprintf(ErrDupOptArg, 'R');
+    
+    // -D was used multiple times with delim information
+    if (n_delim > 1)
+        eprintf(ErrDupOptArg, 'D');
+    
+    // cannot have delims on raw binary input
+    if ((flags & RAW_OPT_MASK) && (flags & DELIM_OPT_MASK))
+        eprintf(ErrOptMutex, 'D', 'R');
+    
+    // cannot have "raw" string, we handle ascii files, it's already raw
+    if ((flags & RAW_OPT_MASK) && (flags & STRING_OPT_MASK))
+        eprintf(ErrOptMutex, 'S', 'R');
+
+    return;
+}
--- ch06/6-04/freq.internal.h
+++ ch06/6-04/freq.internal.h
@@ -5,7 +5,7 @@
 #include <getopt.h>
 
 enum { HELP, AGGR, SORT, DELIM, RAW, CHAR, INT, DOUBLE, STRING };
-//  parse_opts constants
+// parse_opts constants
 #define N_SUPPORTED_OPTS  9 
 #define OPT_STR           "+:hasD:R::cidS"
 
@@ -18,9 +18,10 @@ enum { HELP, AGGR, SORT, DELIM, RAW, CHAR, INT, DOUBLE, STRING };
 #define CHAR_OPT_MASK     ((1<<CHAR))
 #define STRING_OPT_MASK   ((1<<STRING))
 
-//  atoi_pos err values
+// atoi_pos err values
 #define NOT_POSITIVE_INT_ERR  -1
 
+// err messsage strings
 extern const char           *ErrOptReqsArg;
 extern const char           *ErrOptMutex;
 extern const char           *ErrOptMutexDefault;
@@ -28,12 +29,24 @@ extern const char           *ErrDupOptArg;
 extern const char           *ErrMultiSizeRaw;
 extern const char           *ErrInvOpt;
 extern const char           *ErrInvSizeArg;
+
+// usage message string
 extern const char           *UsageInfoStr;
-extern const struct option  LongOpts[]; // getopt.h: struct option
+
+// getopt.h struct opton, option and argument listing, "man 3 getopt_long"
+extern const struct option  LongOpts[];
 
 extern void freq            (FILE *fin, int flags, char *delim, int size);
-extern void usage_exit      (void);
 extern int  parse_opts      (int argc, char *argv[], char **delim, int *size);
 extern int  set_opt_flag    (int flags, int opt);
-extern int  atoi_pos  (char *str);
+extern int  atoi_pos        (char *str);
+extern char *a_readline     (FILE *fin);
+
+
+extern void e_usage           (void);
+extern void e_set_delim       (char **delim, int *n_delim);
+extern void e_set_raw_size    (int *size, int *n_raw);
+extern void e_invalid_opt     (char **argv);
+extern void e_validate_flags  (int flags, int n_raw, int n_delim);
+
 #endif
