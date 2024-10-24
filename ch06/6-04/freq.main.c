67b1d975c031eebb21de5415b48820d1f00837c0
HuseyinAtacanDemir <40474534+HuseyinAtacanDemir@users.noreply.github.com>
2024-10-24 03:29:12 -0400

    REFACTOR ch06 6-04 freq freq.main.c include eread.h
    Refactored freq.main.c to include the eread.h
    
    read logic will be implemented in the main, the data structures will be kept
    and managed in main.
    
    This is done in order to have the option for future parallelism and large file
    support (e_read_buf may need to be called multiple times if it is not feasible
    to just allocate more memory to the exiting buf in case of a really large file)

--- ch06/6-04/freq.main.c
+++ ch06/6-04/freq.main.c
@@ -3,34 +3,42 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <limits.h>
 
 #include "eprintf.h"
+#include "eread.h"
+#include "hash.h"
+#include "qsort.h"
 
 int main(int argc, char **argv)
 {
     setprogname("freq");
 
-    int   fd, size, flags;
-    char  *delim;
+    char      *delim, *buf, char_freq[UCHAR_MAX];
+    int       fd, bufsize, rawsize, flags;
+    Hashmap   *maps[3];
 
-    flags = parse_opts(argc, argv, &delim, &size);
+    flags = parse_opts(argc, argv, &delim, &rawsize);
 
 		// optind: index of next opt in argv, see "man 3 getopt"
     argc -= optind;
     argv += optind;
 
-    if (argc == 0)
-        freq(STDIN_FILENO, flags, delim, size);
-    else
+    if (argc == 0) {
+        bufsize = ea_read_buf(STDIN_FILENO, &buf);
+        freq(buf, bufsize, flags, delim, rawsize);
+    } else {
         for (int i = 0; i < argc; i++) {
             if ((fd = open(argv[i], O_RDONLY)) == -1)
                 eprintf("cannot open file: %s:", argv[i]);
 
-            freq(fd, flags, delim, size);
+            bufsize = ea_read_buf(fd, &buf);
+            freq(buf, bufsize, flags, delim, rawsize);
 
             if (close(fd))
                 eprintf("cannot close file: %s:", argv[i]);
         }
+    }
 
    	exit(EXIT_SUCCESS);
 }
