cb34a9a06b37f57778961503a92141dd0c1b21cc
HuseyinAtacanDemir <40474534+HuseyinAtacanDemir@users.noreply.github.com>
2024-10-23 20:58:21 -0400

    REFACTOR ch06 6-04 freq freq.main.c refactored code to use file descriptors and open and close syscalls instead of fopen and fclose because that's what real men do :D

--- ch06/6-04/freq.main.c
+++ ch06/6-04/freq.main.c
@@ -1,6 +1,6 @@
 #include "freq.internal.h"
 
-#include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <getopt.h>
 
@@ -10,31 +10,25 @@ int main(int argc, char **argv)
 {
     setprogname("freq");
 
-    FILE  *fin;
+    int   fd, size, flags;
     char  *delim;
-    int 	size;
-    int   flags;
 
-    delim = NULL;
     flags = parse_opts(argc, argv, &delim, &size);
-
-    printf("delim: %s, size: %d, flags: %x\n", 
-            (delim ? delim : "NULL"), size, flags);
 
 		// optind: index of next opt in argv, see "man 3 getopt"
     argc -= optind;
     argv += optind;
 
     if (argc == 0)
-        freq(stdin, flags, delim, size);
+        freq(STDIN_FILENO, flags, delim, size);
     else
         for (int i = 0; i < argc; i++) {
-            if ((fin = fopen(argv[i], "r")) == NULL)
+            if ((fd = open(argv[i], O_RDONLY)) == -1)
                 eprintf("cannot open file: %s:", argv[i]);
 
-            freq(fin, flags, delim, size);
+            freq(fd, flags, delim, size);
 
-            if (fclose(fin) == EOF)
+            if (close(fd))
                 eprintf("cannot close file: %s:", argv[i]);
         }
 
